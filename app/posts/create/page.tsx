c3b617c191818f9d544d4b64144ce678d3909ec0
Kris Oud <oudster@gmail.com>
2024-01-20 15:00:27 -0500

    implemented adding posts to mongo

---
+++ app/posts/create/page.tsx
@@ -0,0 +1,18 @@
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import { useState, useRef } from "react";
+
+import AddPost from '@/components/posts/Create'
+export default async function CreatePost() {
+    const userSession = await getServerSession();
+    if (userSession == null) redirect('/')
+
+   
+    return (
+        <div className="font-sans grid w-1/3">
+            
+            <h1>logged in as {userSession.user?.name}</h1>
+            <AddPost />
+        </div>
+      );
+}
